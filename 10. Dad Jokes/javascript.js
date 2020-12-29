402a1f11be9f76ef1b3bd191c68210b73cee254c
tinzuu05 <64735248+tinzuu05@users.noreply.github.com>
2020-12-29 08:42:52 +0000

    day10 done

---
+++ 10. Dad Jokes/javascript.js
@@ -0,0 +1,38 @@
+const jokeEl = document.querySelector('#joke')
+const jokeBtn = document.querySelector('#jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke()
+
+//method 1 USING .then()
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         })
+    
+// }
+
+//method 2 USING ASYNC/AWAIT
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke
+}
